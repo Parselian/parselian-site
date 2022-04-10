db97e6a4525c9c4a037ea737e25b2322d15ee0f0
Parselian <parselian.study@yandex.ru>
2022-04-10 17:01:48 +0300

    made toggle experience slides functionality

--- build/assets/js/main.js
+++ build/assets/js/main.js
@@ -30,8 +30,70 @@ document.addEventListener('DOMContentLoaded', () => {
 
     portfolioSlider.controller.control = portfolioGallerySlider;
   }
-
   portfolioSliderInit();
+
+  const experienceSlider = () => {
+    const pagination = document.querySelector('.experience__slider-dots'),
+      dots = pagination.querySelectorAll('.experience__slider-dot'),
+      paginationMobile = document.querySelector('.experience__select'),
+      paginationListMobile = paginationMobile.querySelector('.experience__select-list'),
+      paginationSelectedSubtitle = paginationMobile.querySelector('.select-box__selected-text-subtitle'),
+      paginationSelectedTitle = paginationMobile.querySelector('.select-box__selected-text-title'),
+      paginationListItems = paginationListMobile.querySelectorAll('.select-box__list-item'),
+      slides = document.querySelectorAll('.experience__slide')
+
+    const slideToggle = (activeDot) => {
+      slides.forEach((slide, slideID) => {
+        slide.classList.remove('experience__slide_active')
+        if (slideID === activeDot) {
+          slide.classList.add('experience__slide_active')
+        }
+      })
+    }
+
+    pagination.addEventListener('click', (e) => {
+      const target = e.target
+
+      if (target.closest('.experience__slider-dot')) {
+        dots.forEach((dot, activeDot) => {
+          dot.classList.remove('experience__slider-dot_active')
+
+          if (target === dot) {
+            dot.classList.add('experience__slider-dot_active')
+
+            slideToggle(activeDot)
+          }
+        })
+      }
+    })
+
+    paginationMobile.addEventListener('click', (e) => {
+      const target = e.target,
+        icon = document.querySelector('.select-box__selected-icon')
+
+      if (target.closest('.experience__select-active')) {
+        paginationListMobile.classList.toggle('select-box__list_visible')
+        icon.classList.toggle('select-box__selected-icon_rotated')
+      } else if (target.closest('.select-box__list-item')) {
+        paginationListMobile.classList.remove('select-box__list_visible')
+        icon.classList.remove('select-box__selected-icon_rotated')
+
+        paginationSelectedSubtitle.textContent = target.closest('.select-box__list-item').children[0].textContent
+        paginationSelectedTitle.textContent = target.closest('.select-box__list-item').children[1].textContent
+
+        paginationListItems.forEach((item, activeItem) => {
+          item.classList.remove('select-box__list-item_selected')
+
+          if (item === target.closest('.select-box__list-item')) {
+            item.classList.add('select-box__list-item_selected')
+
+            slideToggle(activeItem)
+          }
+        })
+      }
+    })
+  }
+  experienceSlider()
 
   const toggleLabels = (selector) => {
     const inputs = document.querySelectorAll(selector)
