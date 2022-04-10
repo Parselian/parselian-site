4f87adfae3ec98510e729689422be558c4344fbd
Parselian <parselian.study@yandex.ru>
2022-04-10 11:06:54 +0300

    made toggle form labels functionality and also wrote first version of my promo-blockquote

--- build/assets/js/main.js
+++ build/assets/js/main.js
@@ -1,33 +1,53 @@
 'use-strict'
 
 document.addEventListener('DOMContentLoaded', () => {
-  const portfolioSlider = new Swiper('.info-slider', {
-    loop: true,
-    effect: 'fade',
-    fadeEffect: {
-      crossFade: true
-    },
-    pagination: {
-      el: '.swiper-pagination',
-      bulletClass: 'info-slider__dot',
-      bulletActiveClass: 'info-slider__dot_active',
-      clickable: true
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  })
+  const portfolioSliderInit = () => {
+    const portfolioSlider = new Swiper('.info-slider', {
+      loop: true,
+      effect: 'fade',
+      fadeEffect: {
+        crossFade: true
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        bulletClass: 'info-slider__dot',
+        bulletActiveClass: 'info-slider__dot_active',
+        clickable: true
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    })
 
-  const portfolioGallerySlider = new Swiper('.gallery__slider', {
-    loop: true,
-    effect: 'fade',
-    fadeEffect: {
-      crossFade: true
-    }
-  })
+    const portfolioGallerySlider = new Swiper('.gallery__slider', {
+      loop: true,
+      effect: 'fade',
+      fadeEffect: {
+        crossFade: true
+      }
+    })
 
-  portfolioSlider.controller.control = portfolioGallerySlider;
-  document.addEventListener('click', (e) => {
-  })
+    portfolioSlider.controller.control = portfolioGallerySlider;
+  }
+
+  portfolioSliderInit();
+
+  const toggleLabels = (selector) => {
+    const inputs = document.querySelectorAll(selector)
+
+    inputs.forEach(item => {
+      item.addEventListener('focusin', (e) => {
+        e.target.parentNode.querySelector('label').classList.add(`form__${selector}-label_focused`)
+      })
+    })
+
+    inputs.forEach(item => {
+      item.addEventListener('focusout', (e) => {
+        e.target.parentNode.querySelector('label').classList.remove(`form__${selector}-label_focused`)
+      })
+    })
+  }
+  toggleLabels('input')
+  toggleLabels('textarea')
 })
